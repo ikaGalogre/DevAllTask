00e1f00c5076ae906fca85d617afdcf9594d982b
nottbott <irakli.galogre18@gmail.com>
2022-10-03 13:01:38 +0400

    modified

--- js/app.js
+++ js/app.js
@@ -1,36 +1,114 @@
-const firstName = document.getElementById('formFirstname');
-const lastName = document.getElementById('formLastName');
-const address = document.getElementById('address');
-const date = document.getElementById('date');
-const male = document.getElementById('male');
-const feMale = document.getElementById('female');
-const button = document.getElementById('button');
-const form = document.getElementById('form');
+const form = document.getElementById('form')
+const firstName = document.getElementById('name')
+const lastName = document.getElementById('lastname')
+const address = document.getElementById('address')
+const date = document.getElementById('date')
+const gender = document.getElementById('gender')
+const about = document.getElementById('about')
+const error = document.getElementById('error')
+const users = document.getElementById('users')
+const btn = document.getElementById('Add-btn')
 
-function validation() {
 
-    const fname = document.getElementById("formFirstname").value;
-    const lname = document.getElementById("formLastname").value;
-    const address = document.getElementById("address").value;
-    const date = document.getElementById('date').value;
-    const male = document.getElementById('male').value;
-    const feMale = document.getElementById('female').value;
-    const button = document.getElementById('button').value;
-    const form = document.getElementById('form').value;
-    //alerts on empty inputs
-    if (fname == "") {
-      alert("Enter First Name");
-    }else if(lname == '') {
-        alert("Enter Last Name");
-    }else if(address == ''){
-        alert("Enter Address");
-    }
-    //creating table
-    
+const inputs = [firstName, lastName, address];
+for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    input.addEventListener('input', function () {
+        if (!hasAppError === true) return
+        error.textContent = "";
+        hasAppError = false;
+    })
+
 }
 
+form.addEventListener('submit', onSumbit);
 
 
-button.addEventListener("click",(e)=>{
-    console.log('clicked');
+let hasAppError = false;
+
+
+function ValidInput(firstName, lastName, address) {
+    if (firstName === "" || lastName === "" || address === "") {
+        return false;
+    }
+    return true;
+}
+
+let ID = 0;
+btn.addEventListener("click", () => {
+    ID++
 });
+
+
+function draw() {
+
+    let infoLocalStorage = JSON.parse(localStorage.getItem('userData'))
+
+    const userContainer = document.createElement("div")
+    userContainer.classList.add("userContainer")
+    userContainer.id = ID
+    
+    const userInfo = document.createElement("div")
+    userInfo.textContent = `
+    
+    Name: ${infoLocalStorage[ID - userContainer.id].name}
+    Surname:  ${infoLocalStorage[ID - userContainer.id].lname}
+    Address:  ${infoLocalStorage[ID - userContainer.id].adress}
+    Date of Birth:  ${infoLocalStorage[ID - userContainer.id].data}
+    gender:  ${infoLocalStorage[ID - userContainer.id].gender}
+    about: ${infoLocalStorage[ID - userContainer.id].about}
+    `
+    userContainer.appendChild(userInfo)
+
+    const deleteButton = document.createElement("div")
+    deleteButton.textContent = "Delete"
+    deleteButton.addEventListener("click", () => {
+        userContainer.remove()
+    })
+    userContainer.appendChild(deleteButton)
+
+    for (let i = 0; i < infoLocalStorage.length; i++) {
+        users.appendChild(userContainer)
+    }
+
+
+}
+
+function onSumbit(event) {
+    event.preventDefault();
+
+    const userName = firstName.value;
+    const userSurname = lastName.value;
+    const userAddress = address.value;
+    const userDate = date.value;
+    const userGender = gender.value;
+    const userAbout = about.value;
+    let userNumber = ID;
+
+    let userData = [];
+    let showData = {
+        name: userName,
+        lname: userSurname,
+        adress: userAddress,
+        data: userDate,
+        sex: userGender,
+        about: userAbout,
+        number: userNumber
+    };
+    userData.push(showData);
+    userData = userData.concat(JSON.parse(localStorage.getItem('userData') || '[]'))
+    localStorage.setItem('userData', JSON.stringify(userData))
+
+
+
+    const isValidValues = ValidInput(userName, userSurname, userAddress);
+
+    if (!isValidValues) {
+        error.textContent = 'ERROR'
+        hasAppError = true;
+        return;
+    }
+    draw();
+}
+
+window.addEventListener('load', draw);
